4470b17c1c3deaf6af247cd49d15a5c6c9580be7
zzyybs <zzyybs@126.com>
2015-08-25 09:27:57 +0800

    new Client.java

---
+++ src/com/atguigu/crm/Client.java
@@ -0,0 +1,12 @@
+package com.atguigu.crm;
+
+public class Client
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Client is request 001");
+	}
+
+}
