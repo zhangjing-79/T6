9496cb1c4cac4a0950dfa562b48e1e7ca7d44fec
zzyybs <zzyybs@126.com>
2015-08-25 09:49:08 +0800

    Update Client.java
    insert

--- src/com/atguigu/crm/Client.java
+++ src/com/atguigu/crm/Client.java
@@ -8,6 +8,7 @@ public class Client
 		// TODO Auto-generated method stub
 		System.out.println("Client is request 001");
 		System.out.println("Github is response 001,服务器端已经收到message001");
+		System.out.println("Githube want to insert......");
 	}
 
 }
