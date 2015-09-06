be39081cf90349d7c504e26d378f97250c8fc264
zzyybs <zzyybs@126.com>
2015-09-06 18:10:39 +0800

    Update Github0423.java
    System.out.println("Github update the product");

--- src/com/atguigu/crm/Github0423.java
+++ src/com/atguigu/crm/Github0423.java
@@ -6,5 +6,6 @@ public class Github0423
 	{
 		System.out.println("welcome to 尚硅谷,0423的亲们s");
 		System.out.println("Github get the infos,程序员01 commit。。。。。。");
+		System.out.println("Github update the product");
 	}
 }
