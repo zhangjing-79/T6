0a08f8c4fc6f38332125ca682b9c7f99886362cd
zzyybs <zzyybs@126.com>
2015-12-19 20:39:44 +0800

    d

--- src/com/atguigu/crm/Client.java
+++ src/com/atguigu/crm/Client.java
@@ -5,15 +5,6 @@ public class Client
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		System.out.println("Client is request 001");
-		System.out.println("Github is response 001,服务器端已经收到message001");
-		System.out.println("Github want to insert......");
-		System.out.println("Client want to update......");
-		System.out.println("Github want to insert222222");
-		System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("999999999999999999999999999");		
-		System.out.println("0423 come in 2015.12.11");
 		System.out.println("local update ..............");
 		System.out.println("github need ..........................");
 	}
