f44a3e7f7d3e22fdf76dfc3af11ad91511b96d38
zzyybs <zzyybs@126.com>
2016-07-07 18:17:21 +0800

    Update Github0423.java

--- src/com/atguigu/crm/Github0423.java
+++ src/com/atguigu/crm/Github0423.java
@@ -5,12 +5,5 @@ public class Github0423
 	public static void main(String[] args)
 	{
 		System.out.println("Github need this block.....");
-		System.out.println("local new commit");
-		System.out.println("local new commit2");
-		System.out.println("Github----------");
-		System.out.println("local第一次提交");
-		System.out.println("github第一次提交收到");
-		System.out.println("cccccccccccccc");
-		System.out.println("33333333333333");
 	}
 }
