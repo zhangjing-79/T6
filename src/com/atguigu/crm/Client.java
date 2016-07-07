8e0e8fa7236d8cfa3b30739056cc7e60138c8497
zzyybs <zzyybs@126.com>
2016-07-07 18:16:55 +0800

    Update Client.java

--- src/com/atguigu/crm/Client.java
+++ src/com/atguigu/crm/Client.java
@@ -5,18 +5,7 @@ public class Client
 
 	public static void main(String[] args)
 	{
-		System.out.println("local update ..............");
-		System.out.println("github need ..........................");
-		System.out.println("localServer update here");
-		System.out.println("github come here 0719");
-		System.out.println("github come here 0816");
-		System.out.println("666666666666666666666");
-		System.out.println("777777777777777777777");
-		System.out.println("1022 add here");
-		System.out.println("xxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("1122 add here");
-		System.out.println("1226 add here");
-		System.out.println("233333333333333333333");
+		System.out.println("welcome......");
 	}
 
 }
